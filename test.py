c1578a41d9977f665ae9d894f971c3fe941f94d3
agiraude <agiraude@student.42.fr>
2020-12-10 23:06:43 +0100

    added memory leak detection

--- test.py
+++ test.py
@@ -16,26 +16,57 @@ ruleNb = 0
 flags = {
 	'T' : False,  #generate only part of tests
 	'D' : False,  #details failed test
-	'A' : False, #details every test
+	'A' : False,  #details every test
 	'C' : False,  #dont delete *.c
-	'E' : False   #add $ at the end of lines
+	'E' : False,  #add $ at the end of lines
+	'B' : False,  #dont delete binary files (usefull for checking for leaks)
+	'L' : False   #check for leaks
 }
+
+leakTester = "san"
 
 GRN = '\033[32m'
 RED = '\033[31m'
 YLW = '\033[33m'
 CYN = '\033[36m'
 NOC = '\033[0m'
+BLU = '\033[34m'
+LGT = '\033[3m' + BLU
 BLD = '\033[1m'
 
 def parseArgs():
 	global flags
 	global rul_path
 	global genSet
+	global leakTester
 
+	t = False
+	l = False
 	for arg in sys.argv:
 		if arg == sys.argv[0]:
 			continue
+		elif t is True:
+			for letter in arg:
+				if letter not in allTypes:
+					print("Wrong paramter : " + arg)
+					exit()
+				genSet = arg
+			else:
+				rul_path = arg
+			t = False
+		elif l is True:
+			if arg == "val" or arg == "san":
+				leakTester = arg
+			else:
+				print("-l argument should be:")
+				print("\"val\" (valgrind) or \"san\" (fsanitize)")
+				exit()
+			l = False
+		elif arg == '-b':
+			flags['B'] = True
+		elif arg == '-l':
+			flags['L'] = True
+			l = True
 		elif arg == '-d':
 			flags['D'] = True
 		elif arg == '-a' :
@@ -46,14 +77,7 @@ def parseArgs():
 			flags['E'] = True
 		elif arg == '-t':
 			flags['T'] = True
-		elif flags.get('T'):
-			for letter in arg:
-				if letter not in allTypes:
-					print("Wrong paramter : " + arg)
-					exit()
-				genSet = arg
-			else:
-				rul_path = arg
+			t = True
 		else:
 			print("Wrong parameter : " + arg)
 			exit()
@@ -91,12 +115,12 @@ def cleanCharArg(i):
 		i = "127"
 	return i
 
-def genArgs(cnv, nbWC):
+def genArgs(cnv, nbWC, mini=False):
 	ints = ["0","1","42","-21"]
 	maxInt = "2147483647"
 	args = []
 	if cnv in "cdiuxX%":
-		if cnv in "diuxX":
+		if cnv in "diuxX" and mini is False:
 			ints.append(maxInt)
 		elif cnv is "%":
 			nbWC -= 1
@@ -163,9 +187,37 @@ def makeRule(fct, rule, nb):
 		rule = rule[:-1]
 	return "\tif (atoi(argv[1]) == "+str(nb)+")\n\t\treturn ("+fct+"("+rule+"));\n"
 
+def buildLeaks(path):
+	miniRules = {}
+	miniSet = "csduxX"
+	miniPrefix = ["%", "%15", "%0", "%*", "%*.*", "%.15", "%.*"]
+	for cnv in miniSet:
+		miniRules[cnv] = []
+		for pfx in miniPrefix:
+			args = genArgs(cnv, pfx.count('*'), mini=True)
+			for av in args:
+				miniRules[cnv].append("\"{}{}\", {}".format(pfx, cnv, av))
+	with open(path, 'w') as file:
+		file.write("#include \"{}\"\n".format(inc_path))
+		file.write("#include <stdio.h>")
+		file.write("\nint main(int argc, char **argv)\n{\n")
+		for r in miniRules:
+			for v in miniRules[r]:
+				file.write("\tft_printf("+ v +"); printf(\"\\n\");\n")
+		file.write("printf(\"@\");\n")
+		#file.write("char *c = malloc(sizeof(char));\n")
+		file.write("}\n");
+	if leakTester == "san":
+		subprocess.run(["gcc", "-g", "-fsanitize=address", path, "-o", path[:-2], "-L", lib_path, "-lftprintf"])
+	else:
+		subprocess.run(["gcc", "-g", path, "-o", path[:-2], "-L", lib_path, "-lftprintf"])
+	if not flags.get('C'):
+		os.remove(path)
+
 def buildCfiles(path, fct):
 	global ruleNb
 
+	print(LGT + "generating {}".format(path) + NOC)
 	with open(path, 'w') as file:
 		file.write("#include <stdio.h>\n")
 		file.write("#include \"{}\"\n".format(inc_path))
@@ -180,6 +232,7 @@ def buildCfiles(path, fct):
 					file.write(makeRule(fct, v, nb))
 				nb += 1
 		file.write("\n\t(void)i;\n\t(void)c;\n}\n")
+	print(LGT + "compiling {}".format(path) + NOC)
 	subprocess.run(["gcc", path, "-o", path[:-2], "-L", lib_path, "-lftprintf"])
 	if flags.get('C') is False:
 		os.remove(path)
@@ -251,12 +304,46 @@ def runTest(progA, progB):
 		totalko += ko
 	print(BLD + CYN + "TOTAL" + NOC)
 	printRes(totalok, totalko)
-	os.remove(progA)
-	os.remove(progB)
+	if not flags.get('B'):
+		os.remove(progA)
+		os.remove(progB)
+
+def runLeakTest(path):
+	if leakTester == "san":
+		lk = subprocess.run(["./" + path], capture_output=True, text=True)
+		out = lk.stderr
+	else:
+		try:
+			lk = subprocess.run(["valgrind", "./" + path], capture_output=True, text=True)
+			out = lk.stderr
+		except:
+			out = "could not run valgrind."
+	if not flags.get('B'):
+		os.remove(path)
+	if not out:
+		print( GRN + "NO LEAKS DETECTED" + NOC)
+	elif leakTester == "san":
+		if flags.get('L'):
+			print(out)
+		else:
+			print(RED + "LEAKS DETECTED" + NOC)
+			print(LGT + "run {} -l for details".format(sys.argv[0]) + NOC)
+	else:
+		print(out)
 
 parseArgs()
 parseConfig()
 genRules()
+<<<<<<< HEAD
+if not flags.get('L'):
+	buildCfiles("ft.c", "ft_printf")
+	buildCfiles("pf.c", "printf")
+	print()
+	runTest("ft", "pf")
+buildLeaks("leaks.c")
+runLeakTest("leaks")
+=======
 buildCfiles("ft.c", "ft_printf")
 buildCfiles("pf.c", "printf")
 runTest("ft", "pf")
+>>>>>>> ea80074551e24a6dddcc38f0517b931354955fac
